5756979c7048167afa230cedc80e9b7d75e1ea8a
Mostafa Meerzad <mostafameerzad@gmail.com>
2025-11-13 15:54:46 +0430

    fix hero section animation speed

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -4,7 +4,7 @@ import Headline from "../Headline";
 const Hero = () => {
   return (
     <section className="w-full h-screen relative max-h-[900px]" id="hero">
-      <Beams rotation={30} lightColor="white" beamHeight={30} beamWidth={3} />
+      <Beams rotation={30} lightColor="white" beamHeight={30} beamWidth={3} speed={1} />
       <Headline />
     </section>
   );
