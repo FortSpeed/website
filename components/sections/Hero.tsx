5d37c4de2da5266bc739c4ecd1b83e8b46141ce0
Mostafa Meerzad <mostafameerzad@gmail.com>
2025-11-19 21:51:56 +0430

    optimize hero section animation

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -7,6 +7,7 @@ import Achievements from "../ui/Achievements";
 import { motion } from "motion/react";
 import { useState } from "react";
 import PricingModal from "../ui/PricingModal";
+import Hyperspeed from "../Hyperspeed";
 
 const Hero = () => {
   // breakpoints
@@ -15,10 +16,10 @@ const Hero = () => {
 
   // dynamic props
   // const rotation = isMobile ? 10 : isTablet ? 20 : 30;
-  const speed = isMobile ? 0.4 : 0.7;
-  const beamHeight = isTablet ? 20 : isMobile ? 20 : 30;
-  const beamWidth = isTablet ? 2.5 : isMobile ? 2 : 3;
-  const lightColor = isTablet ? "#a1a1a1" : isMobile ? "#a0a0a0" : "white";
+  const speed = isTablet ? 0.3 : isMobile ? .2 : 0.7;
+  const beamHeight = isTablet ? 10 : isMobile ? 7 : 18;
+  const beamWidth = isTablet ? 2.5 : isMobile ? 1.35 : 3;
+  const lightColor = isTablet ? "#a1a1a1" : isMobile ? "#a0a0a0" : "#dbdbdb";
 
   const [open, setOpen] = useState(false);
 
@@ -27,19 +28,46 @@ const Hero = () => {
       className="section pt-32 w-full flex flex-col justify-center items-center gap-20 max-h-[900px] "
       id="hero"
     >
-      <div className="absolute w-full h-full top-0">
+      <div className="absolute w-full h-full -top-40">
         <Beams
           rotation={30}
           lightColor={lightColor}
           beamHeight={beamHeight}
           beamWidth={beamWidth}
           speed={speed}
+          // noiseIntensity={2}
         />
+        {/* <Hyperspeed/> */}
+        {/* <div className="relative w-full h-[100vh] overflow-hidden"> */}
+          {/* Video background */}
+          {/* <video
+            autoPlay
+            muted
+            loop
+            playsInline
+            className="absolute inset-0 w-full h-full object-cover "
+          >
+            <source src="/test.webm" type="video/webm" />
+          </video> */}
+            {/* <source src="/videos/hero-bg.mp4" type="video/mp4" /> */}
+
+          {/* Dark overlay for readability */}
+          {/* <div className="absolute inset-0 bg-black/50"></div> */}
+
+          {/* Content */}
+        {/* </div> */}
       </div>
       <Headline className="  left-0 right-0  mx-auto " />
 
-      <motion.div initial={{opacity: 0, scale: 0.85}} animate={{opacity: 1, scale:1, transition:{delay: 1}}} className="flex justify-center items-center gap-5">
-        <InteractiveHoverButton className="md:py-3 text-md max-md:text-sm " onClick={() => setOpen(true)}>
+      <motion.div
+        initial={{ opacity: 0, scale: 0.85 }}
+        animate={{ opacity: 1, scale: 1, transition: { delay: 1 } }}
+        className="flex justify-center items-center gap-5"
+      >
+        <InteractiveHoverButton
+          className="md:py-3 text-md max-md:text-sm "
+          onClick={() => setOpen(true)}
+        >
           Start Your Project
         </InteractiveHoverButton>
         <InteractiveHoverButton className="md:py-3 text-md max-md:text-sm bg-black text-white">
